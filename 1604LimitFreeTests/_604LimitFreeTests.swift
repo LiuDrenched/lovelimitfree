ca3b3ec599a727ff8e644302fc40e096f7809293
千锋 <qianfeng@liujunting.local>
2016-09-26 09:12:48 +0800

    Initial Commit

---
+++ 1604LimitFreeTests/_604LimitFreeTests.swift
@@ -0,0 +1,36 @@
+//
+//  _604LimitFreeTests.swift
+//  1604LimitFreeTests
+//
+//  Created by 千锋 on 16/9/26.
+//  Copyright © 2016年 Alex. All rights reserved.
+//
+
+import XCTest
+@testable import _604LimitFree
+
+class _604LimitFreeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
